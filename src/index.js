bc479c5a62ad50e4cee32387205eee244ef1d624
Josh Rogan <jjjr1122@gmail.com>
2016-05-12 22:07:35 -0400

    add babel

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 "use strict";
 
 import logger from './logger.js';
-// logger.setDivider(false); 
-// logger.setDebug(true); 
-// logger.setCompact(false); 
-
-// class Test{
-
-// }
-
-// logger('test'); 
-// logger(Test); 
 
 export {logger}
